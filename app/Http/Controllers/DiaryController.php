463ea59a2e120aebbdbc47fe93b24218ccf5b147
nommy <win.back.0@gmail.com>
2019-08-15 20:59:01 +0800

    新規投稿画面を作ろう

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -14,4 +14,20 @@ public function index()
     // view/diaries/index.blade.phpを表示
     return view('diaries.index',['diaries' => $diaries]);
 }
+public function create()
+{
+    // views/diaries/create.blade.phpを表示する
+    return view('diaries.create');
+}
+public function store(Request $request)
+{
+    $diary = new Diary(); //Diaryモデルをインスタンス化
+
+    $diary->title = $request->title; //画面で入力されたタイトルを代入
+    $diary->body = $request->body; //画面で入力された本文を代入
+    $diary->save(); //DBに保存
+
+    return redirect()->route('diary.index'); //一覧ページにリダイレクト
+}
+
 }
